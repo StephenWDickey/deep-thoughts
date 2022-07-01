408495889b3118cc2eb5046626ac3804eae5607a
swd <stphndcky22@gmail.com>
2022-07-01 12:50:29 -0700

    created query to graphQL server, imported to Home. Created ThoughtList component, imported to Home. Update JSX to include thoughts data as props.

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,43 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+
+// import packages and functions
+// Apollo Provider is a React component we can pass data through
+// ApolloClient is a constructor function, will make connection to graphQL server
+// InMemoryCache allows ApolloClient to cache API response data
+// createHttpLink will act as middleware for network requests
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+
+// we will establish connection to /graphql endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
+
+// instantiate Apollo Client instance
+// we also create cache object
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+
+
 function App() {
-  return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
-  );
+  
+    return (
+      <ApolloProvider client={client}>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Home />
+          </div>
+          <Footer />
+        </div>
+      </ApolloProvider>
+    );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
+// import ThoughtList component
+import ThoughtList from '../components/ThoughtList';
+
+// import packages and functions
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
 const Home = () => {
+
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  // now we get thoughts from data array
+  // OPTIONAL CHAINING
+  // negates the need to check if object exists before getting properties
+  // if data exists, store in thoughts variable
+  // if data DOESNT exist, return an empty array
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+  
+  
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
       </div>
     </main>
   );
