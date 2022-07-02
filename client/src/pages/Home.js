b07a11514548f75387bdf13a1d99a7b28acc1b06
swd <stphndcky22@gmail.com>
2022-07-02 15:42:47 -0700

    Created new queries for user data. Imported to Home page. Imported AuthService for loggedIn method. Imported FriendList to Home. Updated Home page to show user data.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,12 +5,22 @@ import ThoughtList from '../components/ThoughtList';
 
 // import packages and functions
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+
+import Auth from '../utils/auth';
+
+import FriendList from '../components/FriendList';
+
 
 const Home = () => {
 
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+
+  // put our Auth method into a variable 
+  const loggedIn = Auth.loggedIn();
 
   // now we get thoughts from data array
   // OPTIONAL CHAINING
@@ -18,19 +28,31 @@ const Home = () => {
   // if data exists, store in thoughts variable
   // if data DOESNT exist, return an empty array
   const thoughts = data?.thoughts || [];
+  
   console.log(thoughts);
   
-  
+  // use useQuery for QUERY_ME_BASIC query
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
             <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
           )}
         </div>
+        { loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null }
       </div>
     </main>
   );
