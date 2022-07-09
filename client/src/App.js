164e0210ac84a501424c14763213f620c3131ebf
stevey d <stphndcky22@gmail.com>
2022-07-09 11:01:31 -0700

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ import Signup from './pages/Signup';
 
 // we will establish connection to /graphql endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql'
+  uri: 'http://localhost:3001/graphql'
 });
 
 
